9e1a90261a790cd3c4f13686cd45920ff189119b
Jacob Bexten <jacob.bexten@umontana.edu>
2020-09-17 19:57:53 -0600

    BlogApp2 initial

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, BlogCreateView
 
 
 
 urlpatterns = [
+
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
 
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
 
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView
 
+from django.views.generic.edit import CreateView
+
 
 from .models import Post
 
@@ -21,3 +23,12 @@ class BlogDetailView(DetailView):
     model = Post
 
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+
+    model = Post
+
+    template_name = 'post_new.html'
+
+    fields = ['title', 'author', 'body']
